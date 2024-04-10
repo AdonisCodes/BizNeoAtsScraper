6a663dcf64ced1ecdef818068606a4c07d2120c2
AdonisCodes <business@simonferns.com>
2024-04-10 10:53:46 +0200

    6

--- src/main.py
+++ src/main.py
@@ -58,10 +58,11 @@ def getAllIds():
     all_ids = []
     total_pages = state["previousPage"] + 1
     page = state["previousPage"] or 0
+    previous_max_month = state["previousMaxMonth"]
     time_until_sleep = randint(
         state["minTimeUntilSleep"][0], state["minTimeUntilSleep"][1]
     )
-    while total_pages >= page:
+    while previous_max_month >= 0:
         if time_until_sleep <= 0:
             sleep_total = randint(
                 state["sleepDurationRange"][0], state["sleepDurationRange"][1]
@@ -106,8 +107,8 @@ def getAllIds():
                 ],
                 "stopped": "all",
                 "update": [
-                    0,
-                    99,
+                    previous_max_month - 1,
+                    previous_max_month,
                 ],
                 "tag_filter_option": "all_tag_selected",
                 "candidates_without_forms": False,
@@ -126,30 +127,62 @@ def getAllIds():
         )
         response_json = response.json()
         total_pages = response_json["profilePagination"]["totalPages"]
-        page += 1
-        print(BCOLORS.OKBLUE + f"Page {page} of {total_pages}" + BCOLORS.ENDC)
+        if previous_max_month > 0 and total_pages == 0:
+            page = 0
+            previous_max_month -= 1
+        elif page >= total_pages:
+            previous_max_month -= 1
+            page = 0
+        else:
+            page += 1
+
+        print(BCOLORS.OKPURPLE + f"Page {page} of {total_pages}" + BCOLORS.ENDC)
+        print(
+            BCOLORS.OKBLUE
+            + f"     Current Month Update {previous_max_month}"
+            + BCOLORS.ENDC
+        )
         all_ids.extend(response_json["candidateSlugs"])
         with open("all_ids.txt", "a") as f:
             f.write("\n".join(response_json["candidateSlugs"]) + "\n")
 
         with open("state.json", "w") as f:
+            state["previousMaxMonth"] = previous_max_month
             state["previousPage"] = page
             json.dump(state, f)
 
-        print(BCOLORS.OKGREEN + f"Total IDs: {len(all_ids)}" + BCOLORS.ENDC)
-        random_sleep_wait = randint(state["timeRange"][0], state["timeRange"][0]) / 3
+        prev = []
+        prev_total = 3
+        if len(all_ids) > prev_total:
+            prev = all_ids[-prev_total:]
+            prev_ = []
+            for item in prev:
+                prev_.append(f"{item[:prev_total*3]}...")
+            prev = prev_
+
         print(
             BCOLORS.OKBLUE
-            + f"Sleeping for {random_sleep_wait} seconds to avoid detection!"
+            + f"     Total IDs: {len(all_ids)}"
+            + BCOLORS.ENDC
+            + BCOLORS.OKCYAN
+            + f"    {prev}"
+            + BCOLORS.ENDC
+        )
+        random_sleep_wait = randint(state["timeRange"][0], state["timeRange"][1]) / 2
+        print(
+            BCOLORS.OKBLUE
+            + f"     Sleeping for {random_sleep_wait} seconds to avoid detection!"
             + BCOLORS.ENDC
         )
         time.sleep(random_sleep_wait)
         request_took = time.time() - start
         time_until_sleep -= request_took
-        print(BCOLORS.OKBLUE + f"Request took {request_took} seconds" + BCOLORS.ENDC)
+        print(
+            BCOLORS.OKBLUE + f"     Request took {request_took} seconds" + BCOLORS.ENDC
+        )
         print(
             BCOLORS.OKBLUE
-            + f"Time until next break: {time_until_sleep or 0}"
+            + f"     Time until next break: {time_until_sleep or 0}"
             + BCOLORS.ENDC
         )
 
@@ -160,25 +193,46 @@ def getMemberInfo(id: str) -> dict:
         print(BCOLORS.FAIL + "Authentication Failed!" + BCOLORS.ENDC)
         return None
 
-    url = f"{BASE_URL_ID}/candidates/{id}"
-    response = requests.get(url, headers=headers, cookies=cookies)
+    url = f"{BASE_URL_BASIC}/candidates/{id}"
+    response = requests.get(
+        url,
+        cookies=cookies,
+        headers=headers,
+    )
     if response.status_code < 399:
         response_json = response.json()
         with open("state.json", "w") as f:
             state["previousUser"] = id
             json.dump(state, f)
-        
+
         with open(f"users/{id}.json", "w") as f:
             json.dump(response_json, f)
-        
-        for file in response_json['candidateAttachments']:
-            file_id = file['id']
+
+        for file in response_json["candidateAttachments"]:
+            print(
+                BCOLORS.OKPURPLE + f"    Downloading File {file['file']['name']}",
+                BCOLORS.ENDC,
+            )
+            file_id = file["id"]
             candidate_id = id
-            file_name = file['file']['name']
-            url = file['file']['url'].replace("\u0026", "&")
+            file_name = file["file"]["name"]
+            if (
+                "text" not in file["file"]["type"]
+                and ".txt" not in file["file"]["name"]
+            ):
+                file_name.replace(".txt", ".pdf")
+
+            url = file["file"]["url"].replace("\u0026", "&")
             response = requests.get(url, headers=headers, cookies=cookies)
             with open(f"users/{candidate_id}_{file_id}_{file_name}", "wb") as f:
                 f.write(response.content)
+
+            with open("state.json", "w") as f:
+                state["previousUser"] = id
+                json.dump(state, f)
+
+        del response_json["options"]
+        del response_json["permissions"]
         return response_json
 
     else:
@@ -202,7 +256,22 @@ def main():
     time_until_sleep = randint(
         state["minTimeUntilSleep"][0], state["minTimeUntilSleep"][1]
     )
-    for id in all_ids:
+    all_ids = [id.strip() for id in all_ids]
+    all_ids = [id.strip() for id in all_ids if len(id) > 0]
+    all_ids = list(set(all_ids))
+    all_ids.sort()
+
+    if state["previousUser"] is None:
+        all_ids = all_ids
+    else:
+        all_ids = all_ids[all_ids.index(state["previousUser"]) + 1 :]
+
+    for index, id in enumerate(all_ids):
+        print(
+            BCOLORS.OKGREEN
+            + f"{index}/{len(all_ids)} Getting Info for {id}"
+            + BCOLORS.ENDC
+        )
         if time_until_sleep <= 0:
             sleep_total = randint(
                 state["sleepDurationRange"][0], state["sleepDurationRange"][1]
@@ -212,32 +281,33 @@ def main():
             )
             print(
                 BCOLORS.OKBLUE
-                + f"Taking a break for {sleep_total} seconds"
+                + f"    Taking a break for {sleep_total} seconds"
                 + BCOLORS.ENDC
             )
             print(
                 BCOLORS.OKBLUE
-                + f"Next Break in {time_until_sleep} seconds"
+                + f"    Next Break in {time_until_sleep} seconds"
                 + BCOLORS.ENDC
             )
             time.sleep(sleep_total)
 
         start = time.time()
         user_info = getMemberInfo(id)
-        print(user_info)
-        random_sleep_wait = randint(state["timeRange"][0], state["timeRange"][0])
+        random_sleep_wait = randint(state["timeRange"][0], state["timeRange"][1])
         print(
             BCOLORS.OKBLUE
-            + f"Sleeping for {random_sleep_wait} seconds to avoid detection!"
+            + f"    Sleeping for {random_sleep_wait} seconds to avoid detection!"
             + BCOLORS.ENDC
         )
         time.sleep(random_sleep_wait)
         request_took = time.time() - start
         time_until_sleep -= request_took
-        print(BCOLORS.OKBLUE + f"Request took {request_took} seconds" + BCOLORS.ENDC)
+        print(
+            BCOLORS.OKBLUE + f"    Request took {request_took} seconds" + BCOLORS.ENDC
+        )
         print(
             BCOLORS.OKBLUE
-            + f"Time until next break: {time_until_sleep or 0}"
+            + f"    Time until next break: {time_until_sleep or 0}"
             + BCOLORS.ENDC
         )
 
